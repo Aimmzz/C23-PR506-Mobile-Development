ee2cb66cd82a0d2a323e98f372b10225e7d42432
Aimmzz <rohimkurniawan456@gmail.com>
2023-05-19 20:33:16 +0700

    Update SplashScreenActivity.kt

--- app/src/main/java/com/rohim/skillifyapp/ui/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/rohim/skillifyapp/ui/splash/SplashScreenActivity.kt
@@ -1,12 +1,21 @@
 package com.rohim.skillifyapp.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.rohim.skillifyapp.R
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreenActivity, FirstPageActivity::class.java))
+            finish()
+        }, 2000)
     }
 }
