1d11575877e694d96b79b78f422baedbb98458e6
Aimmzz <rohimkurniawan456@gmail.com>
2023-05-19 19:49:56 +0700

    coba

---
+++ app/src/main/java/com/rohim/skillifyapp/ui/splash/FirstPageActivity.kt
@@ -0,0 +1,12 @@
+package com.rohim.skillifyapp.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rohim.skillifyapp.R
+
+class FirstPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_page)
+    }
+}
